5b73e03f4cc22ca0349d21315e025cbf9c9f9477
cs91chris <cs91chris@voidbrain.me>
2020-04-12 21:19:40 +0200

    optimized imports

--- flask_errors_handler/__init__.py
+++ flask_errors_handler/__init__.py
@@ -1,12 +1,10 @@
 from .version import *
-
 from .exception import ApiProblem
 from .handler import ErrorHandler
-
-from .dispatchers import ErrorDispatcher
-from .dispatchers import DefaultDispatcher
-from .dispatchers import URLPrefixDispatcher
-from .dispatchers import SubdomainDispatcher
-
-from .normalize import BaseNormalize
-from .normalize import DefaultNormalizeMixin
+from .normalize import BaseNormalize, DefaultNormalizeMixin
+from .dispatchers import (
+    ErrorDispatcher,
+    DefaultDispatcher,
+    URLPrefixDispatcher,
+    SubdomainDispatcher
+)
